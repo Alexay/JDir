c02c95a55f192212f715a82cb5c9edcd2b54eb61
Alexay <731@tuta.io>
2015-10-11 01:00:37 +0800

    Created functionality that I had when I was working on the JSAP branch
    *Added the a switch that currently doesn't do anything
    *Fully implemented the b switch
    *Empty arguments work, but not displaying all the information yet

--- src/szu/alex/Main.java
+++ src/szu/alex/Main.java
@@ -1,18 +1,59 @@
 package szu.alex;
 
 import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Path localDir = Paths.get(".");
 
-        OptionParser parser = new OptionParser("b");
-        parser.accepts("b");
+        OptionParser parser = new OptionParser("a::b?*");
 
+        OptionSet options = parser.parse(args);
 
+        parser.accepts("a", "Display all");
+        parser.accepts("b", "Bare output without metadata");
+        parser.accepts("?", "Displays this help prompt");
 
+        if (options.has("?"))
+            parser.printHelpOn(System.out);
+        else {
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(localDir)) {
 
+                if (options.has("b")) {
+                    for (Path file : stream) {
+                        System.out.println(file.getFileName());
+                    }
+                }
+                else if(options.has("a")) {
+                    for (Path file : stream) {
+                        System.out.println(file.getFileName());
+                    }
+                }
+                else {
+                    for (Path file : stream) {
+                        BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
+                        FileTime lastModifiedFileTime = attr.lastModifiedTime();
+                        DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+                        String lastModified = df.format(lastModifiedFileTime.toMillis());
+                        System.out.println(lastModified + "     " + file.getFileName());
+                    }
+                }
 
+            } catch (IOException | DirectoryIteratorException x) {
+                System.err.println(x);
+            }
+        }
 
 
     }
