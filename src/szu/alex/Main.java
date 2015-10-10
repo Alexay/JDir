e03a80c4643807dfd665f07163648f6a79a1bea2
Alexay <731@tuta.io>
2015-10-10 23:48:04 +0800

    Deleted the jars from the libs and just copied the JOpt source code straight into my own package.

--- src/szu/alex/Main.java
+++ src/szu/alex/Main.java
@@ -1,14 +1,19 @@
 package szu.alex;
 
 import joptsimple.OptionParser;
-import joptsimple.OptionSet;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        OptionParser parser = new OptionParser();
-        OptionSet options = parser.parse();
+
+        OptionParser parser = new OptionParser("b");
+        parser.accepts("b");
+
+
+
+
+
+
 
     }
 }
